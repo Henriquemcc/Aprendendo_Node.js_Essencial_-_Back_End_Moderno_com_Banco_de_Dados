4d6df5bf6b6a701f16e8ae145697a253da597fd2
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-05-07 16:47:41 -0300

    Implementando rota para o meu nome

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
+app.get("/henrique", function(req, res) {
+    res.send("Henrique Mendonça Castelar Campos");
+});
+
 // Inicializa o servidor HTTP na porta 3000
 app.listen(3000, function () {
   console.log("Servidor rodando na porta 3000");
