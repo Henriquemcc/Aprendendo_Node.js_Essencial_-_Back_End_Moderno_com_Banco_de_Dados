d0d2e60cfcdf7922c53690521bd7df11bcf5495f
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-05-07 16:56:26 -0300

    Adicionando rota para 'ola' com o nome passado como query string

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ app.get("/henrique", function(req, res) {
     res.send("Henrique Mendonça Castelar Campos");
 });
 
+app.get("/ola", function(req, res) {
+    let nome = req.query.nome;
+    res.send(`Olá ${nome}`);
+});
+
 // Inicializa o servidor HTTP na porta 3000
 app.listen(3000, function () {
   console.log("Servidor rodando na porta 3000");
