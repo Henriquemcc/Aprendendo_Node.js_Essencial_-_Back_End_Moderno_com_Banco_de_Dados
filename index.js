89a71d3ce94f60ad8dd6713c4490fea8c15661f1
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-05-07 16:19:59 -0300

    Copiando código que serve página hello world

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+// Importa o módulo do Express Framework
+const express = require("express");
+
+// Inicializa um objeto de aplicação Express
+const app = express();
+
+// Cria um manipulador da rota padrão
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+// Inicializa o servidor HTTP na porta 3000
+app.listen(3000, function () {
+  console.log("Servidor rodando na porta 3000");
+});
