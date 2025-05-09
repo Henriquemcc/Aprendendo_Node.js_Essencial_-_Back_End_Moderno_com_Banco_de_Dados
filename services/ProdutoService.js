eb33b5edd428b756a0988dd00d4a7684fde1925b
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-05-09 14:48:48 -0300

    Transformando ProdutoService de uma classe para um módulo

--- services/ProdutoService.js
+++ services/ProdutoService.js
@@ -1,35 +1,36 @@
-const Produto = require('../models/Produto');
+const Produto = require("../models/Produto");
 
-class ProdutoService{
-    
-    constructor() {
-        this.produtosRepository = new Array();
-    }
+let produtosRepository = new Array();
 
-    listar() {
-        return this.produtosRepository;
-    }
-
-    buscarPorId(id) {
-        return this.produtosRepository.find((produto) => produto.id == id);
-    }
-
-    cadastrar(produto) {
-        this.produtosRepository.push(produto);
-        return produto
-    }
-
-    atualizar(produto, id) {
-        let produtoAnterior = this.produtosRepository.find((produtoIterado) => produtoIterado.id == id);
-        produtoAnterior.nome = produto.nome
-        produtoAnterior.preco = produto.preco
-        produtoAnterior.detalhes = produto.detalhes
-        return produtoAnterior
-    }
-
-    deletar(id) {
-        let indiceProdutoRemovido = this.produtosRepository.findIndex((produtoIterado, _, _) => produtoIterado.id == id)
-        if (indiceProdutoRemovido > -1)
-            this.produtosRepository.splice(indiceProdutoRemovido, 1);
-    }
+function listar() {
+  return this.produtosRepository;
 }
+
+function buscarPorId(id) {
+  return this.produtosRepository.find((produto) => produto.id == id);
+}
+
+function cadastrar(produto) {
+  this.produtosRepository.push(produto);
+  return produto;
+}
+
+function atualizar(produto, id) {
+  let produtoAnterior = this.produtosRepository.find(
+    (produtoIterado) => produtoIterado.id == id
+  );
+  produtoAnterior.nome = produto.nome;
+  produtoAnterior.preco = produto.preco;
+  produtoAnterior.detalhes = produto.detalhes;
+  return produtoAnterior;
+}
+
+function deletar(id) {
+  let indiceProdutoRemovido = this.produtosRepository.findIndex(
+    (produtoIterado, _, _) => produtoIterado.id == id
+  );
+  if (indiceProdutoRemovido > -1)
+    this.produtosRepository.splice(indiceProdutoRemovido, 1);
+}
+
+module.exports = { listar, buscarPorId, cadastrar, atualizar, deletar }
