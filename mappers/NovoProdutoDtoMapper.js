8342a17e189e22937fb9a3572f84b994b12288e5
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-05-10 07:51:52 -0300

    Adicionando mapeamento

---
+++ mappers/NovoProdutoDtoMapper.js
@@ -0,0 +1,9 @@
+const Produto = require('../models/Produto').Produto;
+
+function map(novoProdutoDto)
+{
+    return new Produto(
+        0, novoProdutoDto.nome,
+        novoProdutoDto.preco,
+        novoProdutoDto.detalhes);
+}
