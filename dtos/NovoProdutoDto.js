4d3ec029ea704e6b08b8689698517d8b7dd0fa28
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-05-10 07:32:37 -0300

    Adicionando data transfer object

---
+++ dtos/NovoProdutoDto.js
@@ -0,0 +1,9 @@
+class NovoProdutoDto {
+    constructor(nome, preco, detalhes) {
+        this.nome = nome
+        this.preco = preco
+        this.detalhes = detalhes
+    }
+}
+
+module.exports = {NovoProdutoDto}
