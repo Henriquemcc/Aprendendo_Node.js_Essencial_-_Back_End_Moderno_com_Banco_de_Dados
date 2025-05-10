d89828dae7eb355d1dbdafe57009f001424a32a3
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-05-10 08:17:37 -0300

    Implementando data transfer object

---
+++ dtos/ProdutoDtoViewer.js
@@ -0,0 +1,10 @@
+class ProdutoDtoViewer {
+    constructor(id, nome, preco, detalhes) {
+        this.id = id
+        this.nome = nome
+        this.preco = preco
+        this.detalhes = detalhes
+    }
+}
+
+module.exports = {ProdutoDtoViewer}
