b468ace36cc3c4499742cb5f9be4119ac9f19dc1
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-05-10 08:23:36 -0300

    Implementando uso do ProdutoViewerDto

--- services/ProdutoDtoService.js
+++ services/ProdutoDtoService.js
@@ -1,23 +1,25 @@
 const ProdutoService = require('./ProdutoService');
 const NovoProdutoDtoMapper = require('../mappers/NovoProdutoDtoMapper');
 const AtualizarProdutoDtoMapper = require('../mappers/AtualizarProdutoDtoMapper');
+const ProdutoDtoViewerMapper = require('../mappers/ProdutoDtoViewerMapper');
 
 function listar() {
-  return ProdutoService.listar();
+  const produtos = ProdutoService.listar();
+  return produtos.map((produto) => ProdutoDtoViewerMapper.map(produto));
 }
 
 function buscarPorId(id) {
-  return ProdutoService.buscarPorId(id);
+  return ProdutoDtoViewerMapper.map(ProdutoService.buscarPorId(id));
 }
 
 function cadastrar(novoProdutoDto) {
   const produto = NovoProdutoDtoMapper.map(novoProdutoDto);
-  return ProdutoService.cadastrar(produto);
+  return ProdutoDtoViewerMapper.map(ProdutoService.cadastrar(produto));
 }
 
 function atualizar(atualizarProdutoDto, id) {
   const produto = AtualizarProdutoDtoMapper.map(atualizarProdutoDto);
-  return ProdutoService.atualizar(atualizarProdutoDto, id);
+  return ProdutoDtoViewerMapper.map(ProdutoService.atualizar(atualizarProdutoDto, id));
 }
 
 function deletar(id) {
