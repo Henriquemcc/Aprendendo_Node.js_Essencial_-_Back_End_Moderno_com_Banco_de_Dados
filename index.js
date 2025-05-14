4d18da3310f3d10facf41d043f33a789340f8697
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-05-14 16:03:59 -0300

    Revert "Criando um router para API"
    This reverts commit aed854a50173c89a121332e61070d654e6b93049.

--- index.js
+++ index.js
@@ -9,27 +9,26 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
 app.use(express.static('public'));
 
-const routerAPI = express.Router();
-
-routerAPI.use(express.urlencoded({ extended: true }));
-routerAPI.use(express.json());
-
-routerAPI.get("/API/", function (req, res) {
+app.get("/API/", function (req, res) {
   res.send("Hello World");
 });
 
-routerAPI.get("/API/henrique", function(req, res) {
+app.get("/API/henrique", function(req, res) {
     res.send("Henrique Mendonça Castelar Campos");
 });
 
-routerAPI.get("/API/ola", function(req, res) {
+app.get("/API/ola", function(req, res) {
     let nome = req.query.nome;
     res.send(`Olá ${nome}`);
 });
 
-routerAPI.get("/API/registro", function (req, res) {
+app.get("/API/registro", function (req, res) {
     // monta um formulário HTML par receber os dados do usuário
     res.send(`
         <form method="POST" action="/API/registro">
@@ -40,24 +39,24 @@ routerAPI.get("/API/registro", function (req, res) {
     `);
 });
 
-routerAPI.post("/API/registro", function (req, res) {
+app.post("/API/registro", function (req, res) {
     res.send(`Registro recebido com sucesso!<br>
         Usuário ${req.body.nome} cadastrado com sucesso!`)
 }
 );
 
-routerAPI.use('/API/cafe', function(req, res) {
+app.use('/API/cafe', function(req, res) {
     res.status(418).send("Sou um bule de chá");
 });
 
-routerAPI.use('/API/produtos', ProdutoRoute);
+app.use('/API/produtos', ProdutoRoute);
 
-routerAPI.use(function(req, res) {
+app.use(function(req, res) {
     console.log("Rota não encontrada")
     res.status(404).send("Rota não encontrada!");
 });
 
 // Inicializa o servidor HTTP na porta 3000
-routerAPI.listen(3000, function () {
+app.listen(3000, function () {
   console.log("Servidor rodando na porta 3000");
 });
