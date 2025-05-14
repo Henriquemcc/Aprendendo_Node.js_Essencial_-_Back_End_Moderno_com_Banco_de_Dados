0dd909dc2476a93f40e215218df9c57ecb9fe88a
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-05-14 17:12:32 -0300

    Implementando migração

--- migrations/20250514200743_create_produtos.js
+++ migrations/20250514200743_create_produtos.js
@@ -3,7 +3,13 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  // crie um arquivo de migração do knex com id, descrição, marca e valor
+    return knex.schema.createTable('produtos', function(table) {
+        table.increments('id').primary();
+        table.string('descricao', 255).notNullable();
+        table.string('marca', 128).notNullable();
+        table.decimal('valor').notNullable();
+    });
 };
 
 /**
